9f440b579ec51d8be5e5b0ec9b5b4c3201c14dbf
Sean Oplinger <sean@soplinger.com>
2024-04-15 14:17:07 -0400

    code fixes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,13 +7,10 @@
 
 import React from "react";
 import Login from "./components/Login";
-import Register from "./components/Register";
 import Welcome from "./components/Welcome";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from "./components/Home";
 import MedicalItems from "./components/MedicalItems";
-import Navigation from "./components/Navigation"; // Import the Navigation component
 import AddMedicalSupply from "./components/AddMedicalSupply"; // Import the AddMedicalSupply component
 
 function App() {
